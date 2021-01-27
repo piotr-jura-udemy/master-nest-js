fdf4fa9c0139f8036a6e7ced07f55bdb02e52ea5
Piotr Jura <>
2021-01-27 18:42:09 +0100

    Request Body

--- src/events.controller.ts
+++ src/events.controller.ts
@@ -1,19 +1,19 @@
-import { Controller, Delete, Get, Param, Patch, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Patch, Post } from "@nestjs/common";
 
 @Controller('/events')
 export class EventsController {
   @Get()
-  findAll() {
-    return 'all';
-  }
+  findAll() { }
   @Get(':id')
   findOne(@Param('id') id) {
     return id;
   }
   @Post()
-  create() { }
+  create(@Body() input) {
+    return input;
+  }
   @Patch(':id')
-  update(@Param('id') id) { }
+  update(@Param('id') id, @Body() input) { }
   @Delete(':id')
   remove(@Param('id') id) { }
 }
