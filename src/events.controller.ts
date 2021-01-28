8e2e18b483f163773a7660ec1be5de1e31230659
Piotr Jura <>
2021-01-28 18:54:27 +0100

    PartialType, Update Data Transfer Object

--- src/events.controller.ts
+++ src/events.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from "@nestjs/common";
 import { CreateEventDto } from './create-event.dto';
+import { UpdateEventDto } from "./update-event.dto";
 
 @Controller('/events')
 export class EventsController {
@@ -22,7 +23,7 @@ export class EventsController {
   }
 
   @Patch(':id')
-  update(@Param('id') id, @Body() input) {
+  update(@Param('id') id, @Body() input: UpdateEventDto) {
     return input;
   }
 
