78500c7de2651a78e256fab80dfae1f4fa9f6a34
Piotr Jura <>
2021-01-30 21:08:54 +0100

    Working API example

---
+++ src/event.entity.ts
@@ -0,0 +1,7 @@
+export class Event {
+  id: number;
+  name: string;
+  description: string;
+  when: Date;
+  address: string;
+}
--- src/events.controller.ts
+++ src/events.controller.ts
@@ -1,33 +1,58 @@
 import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from "@nestjs/common";
 import { CreateEventDto } from './create-event.dto';
+import { Event } from './event.entity';
 import { UpdateEventDto } from "./update-event.dto";
 
 @Controller('/events')
 export class EventsController {
+  private events: Event[] = [];
+
   @Get()
   findAll() {
-    return [
-      { id: 1, name: 'First event' },
-      { id: 2, name: 'Second event' }
-    ];
+    return this.events;
   }
 
   @Get(':id')
   findOne(@Param('id') id) {
-    return { id: 1, name: 'First event' }
+    const event = this.events.find(
+      event => event.id === parseInt(id)
+    );
+
+    return event;
   }
 
   @Post()
   create(@Body() input: CreateEventDto) {
-    return input;
+    const event = {
+      ...input,
+      when: new Date(input.when),
+      id: this.events.length + 1
+    }
+    this.events.push(event);
+    return event;
   }
 
   @Patch(':id')
   update(@Param('id') id, @Body() input: UpdateEventDto) {
-    return input;
+    const index = this.events.findIndex(
+      event => event.id === parseInt(id)
+    );
+
+    this.events[index] = {
+      ...this.events[index],
+      ...input,
+      when: input.when ?
+        new Date(input.when) : this.events[index].when
+    }
+
+    return this.events[index];
   }
 
   @Delete(':id')
   @HttpCode(204)
-  remove(@Param('id') id) { }
+  remove(@Param('id') id) {
+    this.events = this.events.filter(
+      event => event.id !== parseInt(id)
+    );
+  }
 }
