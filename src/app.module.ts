b105b680f4a0629aa33ae9412ee1f22b59f0be29
Piotr Jura <>
2021-02-04 18:39:50 +0100

    Connecting to the Database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventsController } from './events.controller';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: '127.0.0.1',
+    port: 3306,
+    username: 'root',
+    password: 'example',
+    database: 'nest-events'
+  })],
   controllers: [AppController, EventsController],
   providers: [AppService],
 })
