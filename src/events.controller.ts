459807a03182c6b97398ff4b5f2bf4f2f87baf1e
Piotr Jura <>
2021-02-09 19:49:23 +0100

    Querying Criteria and Options

--- src/events.controller.ts
+++ src/events.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
+import { Like, MoreThan, Repository } from "typeorm";
 import { CreateEventDto } from './create-event.dto';
 import { Event } from './event.entity';
 import { UpdateEventDto } from "./update-event.dto";
@@ -15,6 +15,23 @@ export class EventsController {
   @Get()
   async findAll() {
     return await this.repository.find();
+  }
+
+  @Get('/practice')
+  async practice() {
+    return await this.repository.find({
+      select: ['id', 'when'],
+      where: [{
+        id: MoreThan(3),
+        when: MoreThan(new Date('2021-02-12T13:00:00'))
+      }, {
+        description: Like('%meet%')
+      }],
+      take: 2,
+      order: {
+        id: 'DESC'
+      }
+    });
   }
 
   @Get(':id')
