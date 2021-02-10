28ea18d2582b1d8a48e0482c8a925d7cd41033ed
Piotr Jura <>
2021-02-10 17:57:52 +0100

    Validating Input

--- src/create-event.dto.ts
+++ src/create-event.dto.ts
@@ -1,6 +1,13 @@
+import { IsDateString, IsString, Length } from "class-validator";
+
 export class CreateEventDto {
+  @IsString()
+  @Length(5, 255, { message: 'The name length is wrong' })
   name: string;
+  @Length(5, 255)
   description: string;
+  @IsDateString()
   when: string;
+  @Length(5, 255)
   address: string;
 }
