fd90318b99484c6cad0c47b49f6592947e3a3db8
Piotr Jura <>
2021-02-28 13:38:14 +0100

    Custom Decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get, Post, Request, UseGuards } from "@nestjs/common";
+import { Controller, Get, Post, UseGuards } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 import { AuthService } from "./auth.service";
+import { CurrentUser } from "./current-user.decorator";
+import { User } from "./user.entity";
 
 @Controller('auth')
 export class AuthController {
@@ -10,16 +12,16 @@ export class AuthController {
 
   @Post('login')
   @UseGuards(AuthGuard('local'))
-  async login(@Request() request) {
+  async login(@CurrentUser() user: User) {
     return {
-      userId: request.user.id,
-      token: this.authService.getTokenForUser(request.user)
+      userId: user.id,
+      token: this.authService.getTokenForUser(user)
     }
   }
 
   @Get('profile')
   @UseGuards(AuthGuard('jwt'))
-  async getProfile(@Request() request) {
-    return request.user;
+  async getProfile(@CurrentUser() user: User) {
+    return user;
   }
 }
