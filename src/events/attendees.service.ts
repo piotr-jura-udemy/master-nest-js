004b5ff30d72c50fdba2eecddd85dabe14d043c3
Piotr Jura <>
2021-03-05 15:30:23 +0100

    Current User Event Attendance - Business Logic

--- src/events/attendees.service.ts
+++ src/events/attendees.service.ts
@@ -1,7 +1,9 @@
+import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { Attendee } from "./attendee.entity";
 
+@Injectable()
 export class AttendeesService {
   constructor(
     @InjectRepository(Attendee)
@@ -12,5 +14,29 @@ export class AttendeesService {
     return await this.attendeeRepository.find({
       event: { id: eventId }
     });
+  }
+
+  public async findOneByEventIdAndUserId(
+    eventId: number, userId: number
+  ): Promise<Attendee | undefined> {
+    return await this.attendeeRepository.findOne(
+      {
+        event: { id: eventId },
+        user: { id: userId }
+      }
+    );
+  }
+
+  public async createOrUpdate(
+    input: any, eventId: number, userId: number
+  ): Promise<Attendee> {
+    const attendee = await this.findOneByEventIdAndUserId(eventId, userId)
+      ?? new Attendee();
+
+    attendee.eventId = eventId;
+    attendee.userId = userId;
+    // Rest of input...
+
+    return await this.attendeeRepository.save(attendee);
   }
 }
--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -153,4 +153,21 @@ export class EventsService {
     return this.getEventsBaseQuery()
       .where('e.organizerId = :userId', { userId });
   }
+
+  public async getEventsAttendedByUserIdPaginated(
+    userId: number, paginateOptions: PaginateOptions
+  ): Promise<PaginatedEvents> {
+    return await paginate<Event>(
+      this.getEventsAttendedByUserIdQuery(userId),
+      paginateOptions
+    );
+  }
+
+  private getEventsAttendedByUserIdQuery(
+    userId: number
+  ) {
+    return this.getEventsBaseQuery()
+      .leftJoinAndSelect('e.attendees', 'a')
+      .where('a.userId = :userId', { userId });
+  }
 }
