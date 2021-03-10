0e12aa8ddd338498890d72777829acc2d92e9e4f
Piotr Jura <>
2021-03-10 18:33:33 +0100

    Complex Unit Tests Part 2

--- src/events/events.service.spec.ts
+++ src/events/events.service.spec.ts
@@ -7,8 +7,23 @@ import { EventsService } from "./events.service";
 describe('EventsService', () => {
   let service: EventsService;
   let repository: Repository<Event>;
+  let selectQb;
+  let deleteQb;
 
   beforeEach(async () => {
+    deleteQb = {
+      where: jest.fn(),
+      execute: jest.fn()
+    }
+
+    selectQb = {
+      delete: jest.fn().mockReturnValue(deleteQb),
+      where: jest.fn(),
+      execute: jest.fn(),
+      orderBy: jest.fn(),
+      leftJoinAndSelect: jest.fn()
+    };
+
     const module = await Test.createTestingModule({
       providers: [
         EventsService,
@@ -16,7 +31,7 @@ describe('EventsService', () => {
           provide: getRepositoryToken(Event),
           useValue: {
             save: jest.fn(),
-            createQueryBuilder: jest.fn(),
+            createQueryBuilder: jest.fn().mockReturnValue(selectQb),
             delete: jest.fn(),
             where: jest.fn(),
             execute: jest.fn()
@@ -39,6 +54,33 @@ describe('EventsService', () => {
         name: 'New name'
       })).resolves.toEqual({ id: 1 });
       expect(repoSpy).toBeCalledWith({ id: 1, name: 'New name' });
+    });
+  });
+
+  describe('deleteEvent', () => {
+    it('should delete an event', async () => {
+      const createQueryBuilderSpy = jest.spyOn(
+        repository, 'createQueryBuilder'
+      );
+      const deleteSpy = jest.spyOn(
+        selectQb, 'delete'
+      ).mockReturnValue(deleteQb);
+      const whereSpy = jest.spyOn(
+        deleteQb, 'where'
+      ).mockReturnValue(deleteQb);
+      const executeSpy = jest.spyOn(
+        deleteQb, 'execute'
+      );
+
+      expect(service.deleteEvent(1)).resolves.toBe(undefined);
+
+      expect(createQueryBuilderSpy).toHaveBeenCalledTimes(1);
+      expect(createQueryBuilderSpy).toHaveBeenCalledWith('e');
+
+      expect(deleteSpy).toHaveBeenCalledTimes(1);
+      expect(whereSpy).toHaveBeenCalledTimes(1);
+      expect(whereSpy).toHaveBeenCalledWith('id = :id', { id: 1 });
+      expect(executeSpy).toHaveBeenCalledTimes(1)
     });
   });
 });
