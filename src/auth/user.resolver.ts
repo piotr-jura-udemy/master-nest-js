69ec8599cd65d39e64e4a2a2e349d2cd39802097
Piotr Jura <jura.piotrek@gmail.com>
2022-12-25 12:09:57 +0100

    Getting the current user in GQL

---
+++ src/auth/user.resolver.ts
@@ -0,0 +1,18 @@
+import { Query, Resolver } from '@nestjs/graphql';
+import { User } from './user.entity';
+import { CurrentUser } from './current-user.decorator';
+import { UseGuards } from '@nestjs/common';
+import { AuthGuardJwtGql } from './auth-guard-jwt-gql';
+
+@Resolver(() => User)
+export class UserResolver {
+  public async create(): Promise<User> {
+    return new User();
+  }
+
+  @Query(() => User, { nullable: true })
+  @UseGuards(AuthGuardJwtGql)
+  public async me(@CurrentUser() user: User): Promise<User> {
+    return user;
+  }
+}
