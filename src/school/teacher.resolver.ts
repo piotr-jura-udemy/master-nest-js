e1188baae9405fb6cf4dbb013adc45fda48714d0
Piotr Jura <jura.piotrek@gmail.com>
2023-01-02 19:26:40 +0100

    010-012-resolve-field

--- src/school/teacher.resolver.ts
+++ src/school/teacher.resolver.ts
@@ -1,11 +1,22 @@
-import { Resolver, Query, Args, Int, Mutation } from '@nestjs/graphql';
+import {
+  Resolver,
+  Query,
+  Args,
+  Int,
+  Mutation,
+  ResolveField,
+  Parent,
+} from '@nestjs/graphql';
 import { Teacher } from './teacher.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { TeacherAddInput } from './input/teacher-add.input';
+import { Logger } from '@nestjs/common';
 
 @Resolver(() => Teacher)
 export class TeacherResolver {
+  private readonly logger = new Logger(TeacherResolver.name);
+
   constructor(
     @InjectRepository(Teacher)
     private readonly teachersRepository: Repository<Teacher>,
@@ -34,5 +45,11 @@ export class TeacherResolver {
     input: TeacherAddInput,
   ): Promise<Teacher> {
     return await this.teachersRepository.save(new Teacher(input));
+  }
+
+  @ResolveField('subjects')
+  public async subjects(@Parent() teacher: Teacher) {
+    this.logger.debug(`@ResolveField subjects was called`);
+    return await teacher.subjects;
   }
 }
