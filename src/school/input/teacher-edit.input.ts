aa7e740c9349b90831c3bff04b64a97ab4edae5f
Piotr Jura <jura.piotrek@gmail.com>
2023-01-03 19:11:15 +0100

    010-015-partial-omit-type

--- src/school/input/teacher-edit.input.ts
+++ src/school/input/teacher-edit.input.ts
@@ -1,5 +1,7 @@
-import { InputType, PartialType } from '@nestjs/graphql';
+import { InputType, OmitType, PartialType } from '@nestjs/graphql';
 import { TeacherAddInput } from './teacher-add.input';
 
 @InputType()
-export class TeacherEditInput extends PartialType(TeacherAddInput) {}
+export class TeacherEditInput extends PartialType(
+  OmitType(TeacherAddInput, ['gender']),
+) {}
