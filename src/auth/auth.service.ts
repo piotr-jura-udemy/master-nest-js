74cb24f7794fbb50d936a8d6fee01fc702616e3d
Piotr Jura <jura.piotrek@gmail.com>
2023-01-06 17:57:56 +0100

    010-020-extract-auth-logic-to-auth-service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 import { User } from './user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly jwtService: JwtService) {}
+  private readonly logger = new Logger(AuthService.name);
+
+  constructor(
+    private readonly jwtService: JwtService,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
 
   public getTokenForUser(user: User): string {
     return this.jwtService.sign({
@@ -16,5 +24,23 @@ export class AuthService {
 
   public async hashPassword(password: string): Promise<string> {
     return await bcrypt.hash(password, 10);
+  }
+
+  public async validateUser(username: string, password: string): Promise<User> {
+    const user = await this.userRepository.findOne({
+      where: { username },
+    });
+
+    if (!user) {
+      this.logger.debug(`User ${username} not found!`);
+      throw new UnauthorizedException();
+    }
+
+    if (!(await bcrypt.compare(password, user.password))) {
+      this.logger.debug(`Invalid credentials for user ${username}`);
+      throw new UnauthorizedException();
+    }
+
+    return user;
   }
 }
--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -1,37 +1,15 @@
-import { Injectable, Logger, UnauthorizedException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { InjectRepository } from '@nestjs/typeorm';
-import * as bcrypt from 'bcrypt';
 import { Strategy } from 'passport-local';
-import { Repository } from 'typeorm';
-import { User } from './user.entity';
+import { AuthService } from './auth.service';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
-  private readonly logger = new Logger(LocalStrategy.name);
-
-  constructor(
-    @InjectRepository(User)
-    private readonly userRepository: Repository<User>,
-  ) {
+  constructor(private readonly authService: AuthService) {
     super();
   }
 
   public async validate(username: string, password: string): Promise<any> {
-    const user = await this.userRepository.findOne({
-      where: { username },
-    });
-
-    if (!user) {
-      this.logger.debug(`User ${username} not found!`);
-      throw new UnauthorizedException();
-    }
-
-    if (!(await bcrypt.compare(password, user.password))) {
-      this.logger.debug(`Invalid credentials for user ${username}`);
-      throw new UnauthorizedException();
-    }
-
-    return user;
+    return await this.authService.validateUser(username, password);
   }
 }
